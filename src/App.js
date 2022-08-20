33fb3e0fc51fd7e8720dbef1e6ebfb4f3d7c56e3
Armen Marjinyan <armenmarjinyan@MacBook-Pro-Armen.local>
2022-08-20 15:08:40 +0400

    initial

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Modal from "./Modal/Modal";
+import { useState } from "react";
+const App = () => {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <>
+      <h1>Let’s play with modal!</h1>
+      <button onClick={() => setShowModal(true)}>Open Modal</button>
+      <Modal
+        show={showModal}
+        title="Hello World"
+        onClose={() => setShowModal(false)}
+        onSuccess={() => setShowModal(false)}
+      >
+        <h3>This is modal content...</h3>
+        <p>Welcome back </p>
+      </Modal>
+    </>
+  );
+};
+export default App;
